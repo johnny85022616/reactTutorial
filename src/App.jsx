102415c8a5e161a4559903076b147ce5397b1300
johnny_chang <johnny_chang@friday.tw>
2025-08-26 11:56:53 +0800

    useMemo ver1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Item from './components/component/item';
 import UseEffectEample from './components/lifeCycle/useEffectEample';
 import UseEffectFetch from './components/lifeCycle/useEffectFetch';
 import StyledComponentExample from './components/component/StyledComponentExample';
+import UseMemoExample from './components/component/useMemoExample';
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
       {/* useEffect搭配fetch練習  */}
       {/* <UseEffectFetch></UseEffectFetch> */}
       {/* styled component 使用教學  */}
-      <StyledComponentExample></StyledComponentExample>
+      {/* <StyledComponentExample></StyledComponentExample> */}
+      {/* useMemo屬性說明 */}
+      <UseMemoExample></UseMemoExample>
     </>
   )
 }
