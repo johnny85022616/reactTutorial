cbab739a41405832643c1290b6b697a6e2787e24
張裕 <nte.82002@yahoo.com.tw>
2025-08-28 23:00:17 +0800

    新增useRef第三個用途

--- src/components/component/useRefExample.jsx
+++ src/components/component/useRefExample.jsx
@@ -4,17 +4,27 @@ function useRefExample() {
   const inputRef = useRef(null)
   const intervalRef = useRef(null)
   const [count,setCount] = useState(0)
+  const [inputVal, setInputVal] = useState('')
+  const preinputVal = useRef('')
 
   useEffect(()=>{
     inputRef.current.focus() //用途1 取得dom
 
     intervalRef.current = setInterval(()=>{  //用途2 不會依據state改變重新render
       setCount((count) => count + 1)
-      },1000) //用途2 記錄資料
+      },1000)
       return ()=>{
         clearInterval(intervalRef.current)
       }
   },[])
+
+  useEffect(()=>{
+    preinputVal.current  = inputVal //用途3 紀錄state變化前的值
+  },[inputVal])
+
+  function handleInput(e){
+    setInputVal(e.target.value)
+  } 
   
   return (
     <>
@@ -22,6 +32,9 @@ function useRefExample() {
       <input type="text" ref={inputRef} />
       <p>count: {count}</p>
       <button onClick={()=>clearInterval(intervalRef.current)}>停止</button>
+
+      <input type="text" value={inputVal} onInput={handleInput} />
+      <p>前次輸入： {preinputVal.current}</p>
     </>
   )
 }
