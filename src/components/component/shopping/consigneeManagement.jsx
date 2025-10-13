d1d8f4693d1ec76900f82446c6865103cfcb9e8c
張裕 <nte.82002@yahoo.com.tw>
2025-10-14 00:45:42 +0800

    樣式全寫道scss

--- src/components/component/shopping/consigneeManagement.jsx
+++ src/components/component/shopping/consigneeManagement.jsx
@@ -1,54 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import api from './apis/api'
-import nextArrow from "../../../assets/icons/shopping/next_arrow.png";
 import ConsigneeList from './cp/consignee/consigneeList'
-
-const Head = styled.div`
-  width: 100%;
-  line-height: 50px;
-  text-align: center;
-  border-bottom: 1px solid #e0e0e0;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  box-sizing: border-box;
-`;
-const BackIcon = styled.i`
-  width: 44px;
-  height: 44px;
-  background: url(${nextArrow}) 50% / 20px no-repeat;
-`;
-
-const AddBtn = styled.span`
-  font-size: 15px;
-  color: #3f3f3f;
-  font-weight: 500;
-  margin-right: 20px;
-`;
-
-const LoadingMap = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0; 
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0,0,0,0.5);
-  z-index: 999;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  `
-const LoadingText =styled.p`
-  font-size: 1.2rem; 
-  color: white;
-`
-
-const noDataText = styled.div`
-  margin-top: 200px;
-  font-size: 1.8rem;
-`
+import './style/shopping/consigneeManagement/consigneeManagement.scss'
 
 function ConsigneeManagement() {
 
@@ -72,23 +26,23 @@ function ConsigneeManagement() {
   let content;
 
   if (isLoading) {
-    content = <LoadingMap><LoadingText>Loading...</LoadingText></LoadingMap>
+    content = <div className="loadingMap"><p className="loadingText">Loading...</p></div>
   }else{
     if(consigneeList){
       content = <ConsigneeList data={consigneeList}></ConsigneeList>
     }else{
-      content = <noDataText></noDataText>
+      content = <p className="noDataText">無收貨人資訊</p>
     }
   }
 
 
   return (
     <div className="consigneeManagement">
-      <Head>
-        <BackIcon />
+      <div className="head">
+        <i className="backIcon"/>
         <p>收貨人通訊錄管理</p>
-        <AddBtn>新增</AddBtn>
-      </Head>
+        <span className="addBtn">新增</span>
+      </div>
       {content}
     </div>
   );
--- src/components/component/shopping/cp/consignee/consigneeList.jsx
+++ src/components/component/shopping/cp/consignee/consigneeList.jsx
@@ -1,31 +1,16 @@
 import React from "react";
-import styled from "styled-components";
 
-const Clist = styled.ul`
-  list-style: none;
-  background-color: #f4f4f4;
-  min-height: 100vh;
-  list-style-type: none;
-  padding: 10px 0;
-  margin-bottom: 60px;
-`;
-
-const ConsigneeItem = styled.li`
-  background-color: #fff;
-  display: flex;
-  justify-content: space-between;
-  padding: 15px 10px;
-  margin: 10px 10px;
-  border-radius: 8px;
-`;
 
 function ConsigneeList({data}) {
   return <div className="consigneeList">
-    <Clist>
+    <ul>
       {data.map(v=>{
-        return <ConsigneeItem></ConsigneeItem>
+        return <li className="">
+          <div className="left"></div>
+          <div className="right"></div>
+        </li>
       })}
-    </Clist>
+    </ul>
   </div>;
 }
 
