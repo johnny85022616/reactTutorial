bf6e926c71ce16cded7df56c613ed79b78d9e0e1
johnny_chang <johnny_chang@friday.tw>
2025-10-14 11:03:59 +0800

    將共用樣式scss在index.scss引入

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import './style/reset.scss'
 import App from './App.jsx'
 import allReducer from './redux/reducers';
