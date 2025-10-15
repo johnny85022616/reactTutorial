cd7b26a209aceef35986582fb40d3c4a953b386f
johnny85022616 <nte.82002@gmail.com>
2025-10-16 00:37:44 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
 import allReducer from './redux/reducers';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose  } from 'redux';
 import { Provider } from 'react-redux';
 import { thunk } from 'redux-thunk';
 
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   allReducer,
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
