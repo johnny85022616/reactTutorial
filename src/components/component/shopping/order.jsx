b43ec610776b87b84a9d05f346a603111f2f5774
johnny_chang <johnny_chang@friday.tw>
2025-10-23 18:03:46 +0800

    order ver1

---
+++ src/components/component/shopping/order.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Order() {
+  return (
+    <div className='order'>
+      <div class="notice">
+      <p>提醒您:本公司不會主動打電話告知您任何有關付款修改的問題，若接到可疑電話請拒絕回應。</p>
+    </div>
+    </div>
+  )
+}
+
+export default Order
